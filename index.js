d1a10366b063b68a9e0c40651fdde3f13788bd36
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-07-06 00:03:38 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name)
+{
+    cats.push(`Ralph`);
+    return name;
+}
+
+//const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyPrependCat(name)
+{
+    cats.unshift(`Bob`);
+    return name;
+}
+//console.log(destructivelyPrependCat(`Bob`));
+//console.log(cats);
+function destructivelyRemoveLastCat(name)
+{
+    cats.pop(name);
+    return name;
+}
+function destructivelyRemoveFirstCat(name)
+{
+    cats.shift(name);
+    return name;
+}
+
+
+function appendCat(name) {
+    const newCats = [...cats, name];
+    return newCats;
+}
+function prependCat(name) {
+    const newCats = [name, ...cats];
+    return newCats;
+}
+
+function removeLastCat(name) {
+    const newCats = [...cats]
+    newCats.pop();
+    return newCats;
+}
+
+function removeFirstCat(name) {
+    const newCats = [...cats]
+    newCats.shift();
+    return newCats;
+}
